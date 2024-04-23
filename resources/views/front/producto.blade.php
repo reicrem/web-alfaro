b12e953e0a30387b2db9faba15d820a4855649de
Kenywer <kenywerjss17@gmail.com>
2024-04-23 11:47:15 -0400

    implementacion de tailwind

--- resources/views/front/producto.blade.php
+++ resources/views/front/producto.blade.php
@@ -3,20 +3,29 @@
 @section('content')
 
 <div class="product-container">
-            <div class="product-image-bateria">
-                <img src="images/2267.png" alt="Producto">
-            </div>
-            <div class="product-details-bateria">
-                <h2>Nombre del Producto</h2>
-                <h4>Características del Producto</h4>
-                <h5>Nuestra batería de alto rendimiento, con XX mAh, garantiza una potencia duradera para tus dispositivos electrónicos. Su diseño compacto y tecnología de carga rápida la hacen ideal para usarla en cualquier lugar. Además, cuenta con protección avanzada para un uso seguro y confiable en todo momento.</h5>
-                <h6>Precio: $XX.XX</h6>
-                <div class="btn-agregar-carrito">
-                <button>Agregar al Carrito</button>
-                </div>
-            </div>
-        </div>
+    <div class="product-image-bateria">
+        <img src="images/2267.png" alt="Producto" class="product-image img-thumbnail ">
     </div>
+    <div class="product-details-bateria">
+        <h2>Nombre del Producto</h2>
+        <h4>Características del Producto</h4>
+        <h5>Nuestra batería de alto rendimiento, con XX mAh, garantiza una potencia duradera para tus dispositivos electrónicos. Su diseño compacto y tecnología de carga rápida la hacen ideal para usarla en cualquier lugar. Además, cuenta con protección avanzada para un uso seguro y confiable en todo momento.</h5>
+        
+        <form action="#" method="POST">
+            @csrf
+            <div class="product-options">
+                <h6>Precio: $XX.XX</h6>
+                <label for="cantidad">Cantidad:</label>
+                <select id="cantidad" name="cantidad">
+                    @for ($i = 1; $i <= 10; $i++)
+                    <option value="{{ $i }}">{{ $i }}</option>
+                    @endfor
+                </select>
+                <button type="submit" class="btn btn-primary">Agregar al Carrito</button>
+            </div>
+        </form>
+    </div>
+</div>
 
 
 
