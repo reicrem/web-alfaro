36bdfcbeff32dee56114ad93b4417b9b2e98ab0f
Kenywer <kenywerjss17@gmail.com>
2024-04-25 13:23:34 -0400

    arreglo de formulario, velocidad de carga y producto falta slider

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -2,48 +2,26 @@
 
 @section('content')
 
-  <main class="main">
-  <div id="default-carousel" class="relative w-full" data-carousel="slide">
-    <!-- Carousel wrapper -->
-    <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
-        <!-- Item 1 -->
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src="{{ asset('images/slide.jpg') }}" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 object-cover" alt="Slide 1">
+<main class="main">
+    <div id="default-carousel" class="relative w-full slide-index" data-carousel="slide">
+        <!-- Carousel wrapper -->
+        <div class="relative h-96  overflow-hidden rounded-lg md:h-120">
+            <!-- Item 1 -->
+            <div class="duration-700 ease-in-out" data-carousel-item>
+                <img src="images/slide.jpg" class="absolute block w-50 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 object-cover" alt="Slide 1">
+            </div>
+            <!-- Item 2 -->
+            <div class="duration-700 ease-in-out" data-carousel-item>
+                <img src="images/slide2.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 object-cover" alt="Slide 2">
+            </div>
+            <!-- Item 3 -->
+            <div class="duration-700 ease-in-out" data-carousel-item>
+                <img src="images/slide3.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 object-cover" alt="Slide 3">
+            </div>
         </div>
-        <!-- Item 2 -->
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src="{{ asset('images/slide2.jpg') }}" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 object-cover" alt="Slide 2">
-        </div>
-        <!-- Item 3 -->
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src="{{ asset('images/slide3.jpg') }}" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 object-cover" alt="Slide 3">
-        </div>
-    </div>
-    <!-- Slider indicators -->
-    <div class="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
-        <button type="button" class="w-3 h-3 rounded-full bg-white/50 dark:bg-gray-800/50 focus:outline-none" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-        <button type="button" class="w-3 h-3 rounded-full bg-white/50 dark:bg-gray-800/50 focus:outline-none" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-        <button type="button" class="w-3 h-3 rounded-full bg-white/50 dark:bg-gray-800/50 focus:outline-none" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
-    </div>
-    <!-- Slider controls -->  
-    <button type="button" class="absolute top-1/2 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group transform -translate-y-1/2 bg-transparent focus:outline-none" data-carousel-prev>
-        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/50 dark:bg-gray-800/50 group-hover:bg-white/70 dark:group-hover:bg-gray-800/70 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
-            </svg>
-            <span class="sr-only">Previous</span>
-        </span>
-    </button>
-    <button type="button" class="absolute top-1/2 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group transform -translate-y-1/2 bg-transparent focus:outline-none" data-carousel-next>
-        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/50 dark:bg-gray-800/50 group-hover:bg-white/70 dark:group-hover:bg-gray-800/70 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
-            </svg>
-            <span class="sr-only">Next</span>
-        </span>
-    </button>
-</div>
 
+        
+    </div>
 
     <div class="container py-3">
     <div class="section_maintitle wow fadeInDown animated hover" data-wow-duration="1s" data-wow-delay="0s" style="visibility: visible; animation-duration: 1s; animation-delay: 0s; animation-name: fadeInDown;">
@@ -264,5 +242,6 @@ const interval = setInterval(() => {
     }
 }, 2000); //
   </script>
+  
 </main>
 @endsection
--- resources/views/front/producto.blade.php
+++ resources/views/front/producto.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<div class="container mx-auto px-4 py-8">
+<div class="container mx-auto px-4 py-7 conainer-producto">
     <div class="flex justify-center">
         <div class="w-full max-w-3xl bg-white rounded-lg shadow-lg">
             <div class="flex justify-start">
@@ -11,31 +11,31 @@
             <div class="flow-root rounded-lg border border-gray-100 py-3 shadow-sm">
                 <div class="px-6 py-4 contenido-producto">
                     <div class="grid grid-cols-1 gap-1 py-3 even:bg-gray-50 sm:grid-cols-3 sm:gap-4">
-                        <dt class="font-medium text-gray-900"><h3>Nombre del Producto</h3></dt>
-                        <dd class="text-gray-700 sm:col-span-2">Nombre del Producto aquí</dd>
+                        <dt class="font-medium text-gray-900 nombre-producto"><h2>Nombre del Producto</h2></dt>
+                        <dd class="text-gray-700 sm:col-span-2 nombre-producto2">Nombre del Producto aquí</dd>
                     </div>
                     <div class="grid grid-cols-1 gap-1 py-3 even:bg-gray-50 sm:grid-cols-3 sm:gap-4">
-                        <dt class="font-medium text-gray-900"><H3>Precio</H3></dt>
-                        <dd class="text-gray-700 sm:col-span-2">$XX.XX</dd>
+                        <dt class="font-medium text-gray-900 precio-producto"><H3>Precio</H3></dt>
+                        <dd class="text-gray-700 sm:col-span-2 precio-producto2">$XX.XX</dd>
                     </div>
 
-                        <dt class="font-medium text-gray-900"><h3>Características del Producto</h3></dt>
-                        <dd class="text-gray-700 sm:col-span-2">
+                        <dt class="font-medium text-gray-900 carec-producto"><h3>Características del Producto</h3></dt>
+                        <dd class="text-gray-700 sm:col-span- carec-producto2">
                         Nuestra batería de alto rendimiento, con XX mAh, garantiza una potencia duradera para tus dispositivos electrónicos. Su diseño compacto y tecnología de carga rápida la hacen ideal para usarla en cualquier lugar. Además, cuenta con protección avanzada para un uso seguro y confiable en todo momento.
                         </dd>
                     </div>
                     <div class="flex justify-between items-center mt-4 boton-agregar-cantidad">
                         <form action="#" method="POST" class="flex items-center">
                             @csrf
-                            <label for="cantidad" class="mr-2">Cantidad:</label>
-                            <select id="cantidad" name="cantidad" class="border border-gray-300 rounded-md px-3 py-1 focus:outline-none focus:border-blue-500">
+                            <label for="cantidad" class="mr-2 mt-1">Cantidad:</label>
+                            <select id="cantidad" name="cantidad" class="border border-gray-300 rounded-md px-3 py-1 focus:outline-none">
                                 @for ($i = 1; $i <= 10; $i++)
                                 <option value="{{ $i }}">{{ $i }}</option>
                                 @endfor
                             </select>
                             <button type="button" class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl
                              focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg 
-                             text-sm px-5 py-2.5 text-center me-2 mb-2">Agregar al carrito</button>
+                             text-xl px-5 py-2.5 text-center me-2 ml-3">Agregar al carrito</button>
 
                         </form>
                     </div>
